910c3a75dd6e080db78c2d408966ce4087edab73
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-10-28 22:21:48 +0530

    style: custom chakra styles

---
+++ src/ChakraStyles.js
@@ -0,0 +1,61 @@
+export const HeaderFlex = {
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+
+  width: "100%",
+};
+
+export const AttachmentPopover = {
+  position: "relative",
+  as: "div",
+  display: "flex",
+  flexDirection: "row",
+  gap: "1rem",
+  background: "#008000",
+  padding: "0.75rem 1rem",
+  width: "max-content",
+  borderRadius: "62.4375rem",
+  boxShadow: "0px -1px 8px 0px rgba(0, 0, 0, 0.05)",
+  outline: "none",
+  transform: "translate3d(-55px, -40px, 0px)",
+};
+
+export const EllipsisPopover = {
+  as: "div",
+  display: "flex",
+  flexDirection: "column",
+  width: "max-content",
+  borderRadius: "0.5rem",
+  transform: "translate3d(170px, 115px, 0px)",
+};
+
+export const EllipsisPopoverItems = {
+  width: "9.75rem",
+  height: "3rem",
+  padding: "0.4375rem 0.75rem",
+  alignItems: "center",
+  gap: "0.75rem",
+  borderBottom: "1px solid lightgray",
+};
+
+export const Chat = {
+  flexDirection: "row",
+  gap: "0.5rem",
+  padding: "0 1rem",
+};
+
+export const senderImage = {
+  width: "26px",
+  height: "26px",
+  borderRadius: "100%",
+};
+
+export const ChatMessage = {
+  borderRadius: "0rem 0.75rem 0.75rem 0.75rem;",
+  background: "white",
+  boxShadow: "0px 4px 8px 0px rgba(0, 0, 0, 0.08)",
+  padding: "0.5rem",
+  color: "#606060",
+  fontSize: "0.875rem",
+};
