060c9b2815d5a9096248de7857456d92dc1e8543
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-10-28 22:18:11 +0530

    feat: Header & SendMsg in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { Flex } from "@chakra-ui/react";
 import "./App.css";
+import Header from "./components/Header";
+import SendMsg from "./components/SendMsg";
 
 function App() {
-  return <div></div>;
+  return (
+    <Flex flexDirection="column" paddingBottom="3.5rem">
+      <Header />
+
+      <SendMsg />
+    </Flex>
+  );
 }
 export default App;
